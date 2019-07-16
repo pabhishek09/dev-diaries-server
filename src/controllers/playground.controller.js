8ce64d16cf794de870d5b677514cad98a6db5e88
Pandey <abhishekpandey7@deloitte.com>
2019-07-17 00:12:37 +0530

    added get challenge skeleton

--- src/controllers/playground.controller.js
+++ src/controllers/playground.controller.js
@@ -4,13 +4,26 @@ import _get from 'lodash/get';
 const PlaygroundController = {
 
   getAllChallenges: async (req, res, next) => {
-
+    console.log("Inside PlaygroundController: getAllChallenges");
+    try {
+      const challenges = await PlaygroundService.getAllChallenges();
+      console.log("Exiting PlaygroundController: getAllChallenges");
+      res.send(challenges);
+    } catch (err) {
+      next(err);
+    }
   }, 
   
   getChallengeById: async(req, res, next) => {
-    const challengeId = _get(req, "params.id");
-    console.log("Challenge", challengeId);
-    res.send(challengeId);
+    console.log("Inside PlaygroundController: getChallengeById");
+    try {
+      const challengeId = _get(req, "params.id");
+      const challenge = await PlaygroundService.getChallengeById(challengeId);
+      console.log("Exiting PlaygroundController: getChallengeById");
+      res.send(challenge);
+    } catch (err) {
+      next(err);
+    }
   }, 
 
   createChallenge: async (req, res, next) => {
