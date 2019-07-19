d308b467e6f646bdd07722fa6ef2922f275bdcb8
aashankar <aashrit93@gmail.com>
2019-07-19 13:31:03 +0530

    fixed search

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -1,29 +1,33 @@
 /* eslint-disable consistent-return */
 import recommender from 'recommender';
 import QuestionsService from '../services/questions.service';
-
+import errorResponses from '../common/errorResponses';
 // get the list of questions from mongo
 const SearchController = {
   returnClosestMatches: async (req, res, next) => {
     try {
       const documents = await QuestionsService.getAllQuestions();
-      const titlesOnly = documents.map(data => {
-        return data.title;
-      });
-      const { searchQuery } = req ? req.query : null;
-      recommender.tfidf(searchQuery, titlesOnly, sortedDocs => {
-        console.log(sortedDocs);
-        const sortedDocList = sortedDocs.slice(0, 5);
-        // eslint-disable-next-line array-callback-return
-        const searchResponse = documents.map(doc => {
-          if (sortedDocList.includes(doc.title)) {
-            return doc;
-          }
+      if (documents && documents.length) {
+        const titlesOnly = documents.map(data => {
+          return data.title;
         });
-        res.json({
-          suggested: searchResponse
+        const { searchQuery } = req ? req.query : null;
+        recommender.tfidf(searchQuery, titlesOnly, sortedDocs => {
+          console.log(sortedDocs);
+          const sortedDocList = sortedDocs.slice(0, 5);
+          // eslint-disable-next-line array-callback-return
+          const searchResponse = documents.map(doc => {
+            if (sortedDocList.includes(doc.title)) {
+              return doc;
+            }
+          });
+          res.json({
+            suggested: searchResponse
+          });
         });
-      });
+      } else {
+        res.status(404).json(errorResponses.NotFound);
+      }
     } catch (error) {
       next(error);
     }
